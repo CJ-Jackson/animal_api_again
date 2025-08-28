1974ccb552c28576852795d221cdb4558bc84047
Christopher Jackson <cjjackson@seadra.local>
2025-08-28 08:43:12 +0100

    Clean up

--- backend_api/src/animal/object.rs
+++ backend_api/src/animal/object.rs
@@ -29,8 +29,16 @@ pub struct AnimalErrorObject {
 impl From<AnimalValidationError> for AnimalErrorObject {
     fn from(value: AnimalValidationError) -> Self {
         Self {
-            species: value.species.err().map(|v| v.0.iter().map(|s| s.clone()).collect() ).unwrap_or_default(),
-            description: value.description.err().map(|v| v.0.iter().map(|s| s.clone()).collect() ).unwrap_or_default(),
+            species: value
+                .species
+                .err()
+                .map(|v| v.0.iter().map(|s| s.clone()).collect())
+                .unwrap_or_default(),
+            description: value
+                .description
+                .err()
+                .map(|v| v.0.iter().map(|s| s.clone()).collect())
+                .unwrap_or_default(),
         }
     }
 }
